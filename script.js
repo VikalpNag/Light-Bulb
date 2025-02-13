b2178dbb96d776f025c8ea4f42c7a2f792c50159
Vikalp Nag <vikalpnag@gmail.com>
2025-02-13 20:08:36 +0530

    project completion

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let isOpen = false;
 let btn = document.querySelector(".switch");
+let bulbTop = document.querySelector(".bulb-top");
+let bulbBottom = document.querySelector(".bulb-bottom");
 btn.addEventListener("click", function () {
   if (isOpen) {
     btn.classList.remove("switch-on");
+    bulbTop.classList.remove("bulbOn");
+    bulbBottom.classList.remove("bulbOn");
   } else {
     btn.classList.add("switch-on");
+    bulbTop.classList.add("bulbOn");
+    bulbBottom.classList.add("bulbOn");
   }
   isOpen = !isOpen;
 });
